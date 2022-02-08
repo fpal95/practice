320b93d493e53994313917eb6c48486ad7e510ab
ykk <snc7303@gmail.com>
2022-02-08 13:44:54 +0900

    Implement Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,11 @@
+import java.util.Arrays;
+import java.util.List;
+import logic.BubbleSort;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        BubbleSort<String> bubbleSort = new BubbleSort<>();
+        List<String> list = bubbleSort.sort(Arrays.asList(args));
+        System.out.println("Hello World! - result: " + list);
     }
 }
